e9b8d2c192f98667cd4d2e7bbe4f2354ed3e373d
Samuel_bonifacio <153608752+SAMUELBONIF@users.noreply.github.com>
2026-01-09 17:15:08 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,2 @@
 // src/config.js
-
-// 1. Pegamos o que vem do Vercel
-const rawUrl = import.meta.env.VITE_API_URL || '';
-
-// 2. Se a URL não contiver o sufixo necessário, nós adicionamos manualmente
-// Isso é uma proteção caso a variável do Vercel venha errada
-export const API_URL = rawUrl.includes('/ADUMI/api') 
-  ? rawUrl 
-  : 'https://adumi-backend.onrender.com/ADUMI/api/';
-
-console.log("URL Final que o Axios vai usar:", API_URL);
+export const API_URL = 'https://adumi-backend.onrender.com/ADUMI/api/';
