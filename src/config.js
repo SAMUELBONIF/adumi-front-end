09932abdd2281ce1261513bc41c8ab096a81797d
Samuel_bonifacio <153608752+SAMUELBONIF@users.noreply.github.com>
2026-01-09 16:16:03 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
 // Pegamos a URL do backend da variável de ambiente definida no Vercel
-export const API_URL = import.meta.env.VITE_APP_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL || 'https://adumi-backend.onrender.com/';
