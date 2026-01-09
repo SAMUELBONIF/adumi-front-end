07ca39f5cf54b5f18ba1680f8c283cfb4dc56a69
Samuel_bonifacio <153608752+SAMUELBONIF@users.noreply.github.com>
2026-01-09 16:50:42 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 
-// Adicionamos o caminho completo que vimos na imagem
+// Adicione /ADUMI/api ao final da URL
 export const API_URL = import.meta.env.VITE_API_URL || 'https://adumi-backend.onrender.com/ADUMI/api';
 
 console.log("Conectando na API em:", API_URL);
